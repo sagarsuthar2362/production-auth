ec9be127bd636ef8be1140700b4ded73a2695830
sagarsuthar2362 <1studywithsagar2362@gmail.com>
2026-06-28 22:52:32 +0530

    Fix user field requirement in session schema; ensure user is always required

--- src/models/session.model.js
+++ src/models/session.model.js
@@ -5,6 +5,7 @@ const sessionSchema = new mongoose.Schema(
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+      required: true,
     },
     refreshTokenHash: {
       type: String,
