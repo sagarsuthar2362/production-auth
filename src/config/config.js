22a6a7f57f73b4f3c49a993ce68eb52fc70ea845
sagarsuthar2362 <1studywithsagar2362@gmail.com>
2026-06-29 17:30:49 +0530

    Add nodemailer dependency and update environment variable configuration

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const requiredEnvVars = ["MONGO_URI", "JWT_SECRET"];
+const requiredEnvVars = [
+  "MONGO_URI",
+  "JWT_SECRET",
+  "GOOGLE_CLIENT_ID",
+  "GOOGLE_CLIENT_SECRET",
+  "GOOGLE_REFRESH_TOKEN",
+  "GOOGLE_USER",
+];
 
 const missingEnvVars = requiredEnvVars.filter((envVar) => !process.env[envVar]);
 
@@ -12,6 +19,10 @@ if (missingEnvVars.length > 0) {
 const config = {
   MONGO_URI: process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  GOOGLE_REFRESH_TOKEN: process.env.GOOGLE_REFRESH_TOKEN,
+  GOOGLE_USER: process.env.GOOGLE_USER,
 };
 
 export default config;
